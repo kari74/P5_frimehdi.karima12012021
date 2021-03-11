5e38295510c0aed30729572ff28ff41ff5b8ef54
karima frimehdi <frimehdi.karima@gmail.com>
2021-03-11 10:31:31 +0100

    formulaire

--- frontend/js/plantest.js
+++ frontend/js/plantest.js
@@ -1,24 +1,26 @@
-//function ajax = connexion API
-//function countProductsInCart = comptage du nombre de produit mis dans le panier
-//function disableSubmitButton = bouton "valider" inactif si formulaire invalide 
-//function displayProduct = recuperation/affichage produit
+//function ajax = recuperation de données
+//function countProductsInCart =affichage du nombre de produit mis dans le panier
+//function disableSubmitButton = desactive bouton "valider" inactif si formulaire invalide 
+//function displayProduct = affichage produit
 //function displayProducts = affichage et recuperation des produits
 // function displayTotalInHeader = affichage qty dans le header
-//function displayTeddy(teddy) affichage produits panier 
+//function displayTeddy(teddy) = affichage produits panier 
 //function enableSubmitButton = bouton "valider" actif si formulaire valide
 // function get = recupération des element depuis le localStorage
 //function getUrlValue = recuperation de la clé (id)Url 
 // function hasProductsInCart = presence de produit dans le panier
-//function hide = absence de produit dans le panier
-//function listenForAddition = ajout produit "+"
+//function hide = cache element
+//function isStringValid = verifier la conformité formulaire
+//function listenForAddition = ecoute le click ajout produit "+"
 //function listenForCartAddition = ecoute de l'ajout produit dans le panier au clique
 //function listenForCartEmpty = vider le panier
 //function listenForCartSubmission = soumission du formulaire
 //function listenForDeletion = suppression produit "-"
 //function productExists = verication de l'existance du produit dans le panier 
-//function render =// Fonction permettant d'afficher les produits, ciblés grâce aux paramètres teddies et type ainsi que l'HTML
-//function show = presence de produit dans le panier
+//function render = Fonction permettant d'afficher les produits, ciblés grâce aux paramètres teddies et type ainsi que  la creation du HTML
+//function show = montrer les element 
 // function store = stockage des elements dans le localStorage
+//function validateEmail = vérifier  que l'email =ok 
 //function validefom = verification du formulaire avant validation 
 
 
